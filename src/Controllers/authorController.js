0cd49cf6f8dcda96722b109126c7ff8b54fdf792
soubhagya-jitu <soubhagyasamal03@gmail.com>
2022-09-06 00:09:06 +0530

    by soubhagya

--- src/Controllers/authorController.js
+++ src/Controllers/authorController.js
@@ -1,13 +1,34 @@
-const AuthorModel = require("../Models/authorModel")
+const authorModel = require("../Models/authorModel")
 
 const createAuthor = async function (req, res) {
     try {
         let author = req.body
-        let authorCreated = await AuthorModel.create(author)
-        res.status(201).send(authorCreated)
+        let{fname,lname,title,email,password} = author
+        if(!fname){return res.status(400).send({msg:"fname is required"})}
+        if(!lname){return res.status(400).send({msg:"lname is required"})}
+        if(!title){return res.status(400).send({msg:"title is required"})}
+        if(!email){return res.status(400).send({msg:"email is required"})}
+        if(!password){return res.status(400).send({msg:"password is required"})}
+        
+        let findEmail = await authorModel.findOne({email})
+
+        if (findEmail) {
+           return res.status(400).send({ msg: "email id already exsits" })
+        }
+        let validEmail = function (mail) 
+        {
+         return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+        }
+        if (!validEmail(email)) return res.status(400).send({status:false, msg:"email is not valid"})
+        
+      
+            let authorCreated = await authorModel.create(author)
+            return res.status(201).send(authorCreated)
+        
+
 
     } catch (error) {
-        res.status(500).send({ status: false, msg: error.message })
+        return res.status(500).send({ status: false, msg: error.message })
     }
 
 }
