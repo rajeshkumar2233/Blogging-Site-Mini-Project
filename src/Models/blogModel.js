7b35f1f84e0a3f53ca7a8245aa37b958f3cedf32
rajeshmajhi2233 <rajeshmajhi2233@gmail.com>
2022-09-05 16:37:05 +0530

    project1

---
+++ src/Models/blogModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const blogSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            
+        },
+        body: {
+            type: String,
+            required: true
+        },
+        authorId: {
+            type: ObjectId,
+            refs: ' Author',
+            required: true,
+           
+        },
+        tags: [{
+            type:String, 
+            
+        }],
+        category: {
+            type: String,
+            required: true, 
+            
+        },
+        subcategory: String, 
+           
+        
+        isPublished: {
+            type: Boolean,
+            default: false
+        },
+        publishedAt: {
+            type: Date,
+            default:null
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false
+        },
+        deletedAt: {
+            type: Date,
+            default:null
+        }
+    },
+    { timestamps: true }
+)
+module.exports = mongoose.model('Blogs', blogSchema)
