3cb80d9f2b9d196688e00de8a6f4cacf6b3d26de
ashanka710 <ashankad343@gmail.com>
2022-09-06 22:24:47 +0530

    ashanka

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const authorController = require("../Controllers/authorController")
 const blogController = require("../Controllers/blogController")
 
-router.get("/test-me", function (req, res) {
+router.get("/test-me", function(req, res) {
     res.send("My first ever api!")
 })
 
@@ -13,13 +13,13 @@ router.post("/authors", authorController.createAuthor)
 
 //create blog
 router.post("/blogs", blogController.createBlog)
-// delete blog id
-router.delete('/blogs/:blogId',blogController.deletebyBlogId)
+    // delete blog id
+router.delete('/blogs/:blogId', blogController.deletebyBlogId)
 
 //update blogg
 
 router.put("/blogs/:blogId", blogController.updateBlog)
-router.delete("/blogs",blogController.deleteBlog)
-
+router.delete("/blogs", blogController.deleteBlog)
+router.get("/getBlogs", blogController.getBlogs)
 
 module.exports = router
