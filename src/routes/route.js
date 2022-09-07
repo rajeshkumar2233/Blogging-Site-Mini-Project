3bcb893f9ed1d38f62f9f949f8d4691664c19cb3
rajeshmajhi2233 <rajeshmajhi2233@gmail.com>
2022-09-07 14:05:57 +0530

    by rajesh

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const authorController = require("../Controllers/authorController")
 const blogController = require("../Controllers/blogController")
 
-router.get("/test-me", function(req, res) {
+router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
@@ -13,7 +13,7 @@ router.post("/authors", authorController.createAuthor)
 
 //create blog
 router.post("/blogs", blogController.createBlog)
-    // delete blog id
+// delete blog id
 router.delete('/blogs/:blogId', blogController.deletebyBlogId)
 
 //update blogg
